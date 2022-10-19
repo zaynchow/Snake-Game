c60faf631c8c6cbbe8edb28bf2aa43222e3cb384
zaynchow <83538743+zaynchow@users.noreply.github.com>
2022-10-18 22:53:22 -0700

    implemented Draw

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-//Properties
 
 bool gameOver;
-int width = 20;
-int height =  20;
+
+int height =  5;
+int width = height + 2;
 int pos_x, pos_y, fruitX, fruitY, score;
 enum direction {STOP = 0 , LEFT, RIGHT, UP,DOWN};
 direction dir;
@@ -25,9 +25,25 @@ void Setup(){
 
 void Draw(){
     system("clear");
-    // for (int i = 0; i < width ; i++){
 
-    // }
+
+    for (int j = 0; j < height ; j++){
+        for (int k = 0; k < width; k++){
+            if (j == 0 || j == height-1) {
+                cout<<"#";
+            }
+            else if (k==0 || k == width - 1){
+                cout<<"#";
+            }
+            else {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+
+
+
 
 }
 
@@ -37,13 +53,15 @@ void Logic(){}
 
 
 int main() {
-    Setup();
-    while(!gameOver){
-        Draw();
-        Input();
-        Logic();
+    // Setup();
+    Draw();
 
-    }
+    // while(!gameOver){
+    //     Draw();
+    //     Input();
+    //     Logic();
+
+    // }
  
     return 0;
 }
