344d2eb99361958f418d1af2a2ef66e8cc66810c
zaynchow <83538743+zaynchow@users.noreply.github.com>
2022-10-18 22:00:36 -0700

    Implemented Setup

--- main.cpp
+++ main.cpp
@@ -6,22 +6,30 @@ using namespace std;
 bool gameOver;
 int width = 20;
 int height =  20;
-int pos_x, pos_y, fruitX, fruitY;
+int pos_x, pos_y, fruitX, fruitY, score;
 enum direction {STOP = 0 , LEFT, RIGHT, UP,DOWN};
 direction dir;
 
 
 
 void Setup(){
-    // gameOver= false;
-    // dir = STOP;
-    // pos_x = width/2;
-    // pos_y
-
+    gameOver= false;
+    dir = STOP;
+    pos_x = width/2;
+    pos_y = height/2;
+    fruitX = rand() % width;
+    fruitX = rand() % height;
+    score = 0;
 
 }
 
-void Draw(){}
+void Draw(){
+    system("clear");
+    // for (int i = 0; i < width ; i++){
+
+    // }
+
+}
 
 void Input(){}
 
@@ -34,6 +42,7 @@ int main() {
         Draw();
         Input();
         Logic();
+
     }
  
     return 0;
