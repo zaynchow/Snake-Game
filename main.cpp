cdeaa8fb8854c565dbbad1b3f05d6eea87e07fb7
zaynchow <83538743+zaynchow@users.noreply.github.com>
2022-10-18 21:51:02 -0700

    declared global variables and functions

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 using namespace std;
 
+//Properties
+
+bool gameOver;
+int width = 20;
+int height =  20;
+int pos_x, pos_y, fruitX, fruitY;
+enum direction {STOP = 0 , LEFT, RIGHT, UP,DOWN};
+direction dir;
+
+
 
 void Setup(){
-    
+    // gameOver= false;
+    // dir = STOP;
+    // pos_x = width/2;
+    // pos_y
+
+
 }
 
 void Draw(){}
@@ -14,5 +29,12 @@ void Logic(){}
 
 
 int main() {
+    Setup();
+    while(!gameOver){
+        Draw();
+        Input();
+        Logic();
+    }
+ 
     return 0;
 }
